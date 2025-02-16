110ec95d8a43d1f18e5b954fc2e48fdb4e7f7e07
Galih Tri Risky Andiko <galihtririskyandiko@gmail.com>
2025-02-16 21:18:08 +0700

    second commit

--- lib/presentation/screen/home/home_screen.dart
+++ lib/presentation/screen/home/home_screen.dart
@@ -1,10 +1,66 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fitmom_guide/core/utils/dimensions.dart';
 import 'package:flutter/material.dart';
+import '../../../core/utils/my_images.dart';
 import 'widget/cover_image.dart';
-import 'widget/profile_header.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String _name = "Loading...";
+  String _timeAgo = "Just now";
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserData();
+  }
+
+  void _fetchUserData() async {
+    try {
+      User? user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        DocumentSnapshot userDoc = await FirebaseFirestore.instance
+            .collection("users")
+            .doc(user.uid)
+            .get();
+
+        if (userDoc.exists) {
+          setState(() {
+            _name = userDoc["name"] ?? "No Name";
+            _timeAgo =
+                _calculateTimeAgo(user.metadata.creationTime ?? DateTime.now());
+          });
+        }
+      }
+    } catch (e) {
+      setState(() {
+        _name = "Error loading name";
+      });
+    }
+  }
+
+  String _calculateTimeAgo(DateTime time) {
+    Duration difference = DateTime.now().difference(time);
+    if (difference.inDays > 30) {
+      return "${(difference.inDays / 30).floor()} month(s) ago";
+    } else if (difference.inDays > 0) {
+      return "${difference.inDays} day(s) ago";
+    } else if (difference.inHours > 0) {
+      return "${difference.inHours} hour(s) ago";
+    } else if (difference.inMinutes > 0) {
+      return "${difference.inMinutes} minute(s) ago";
+    } else {
+      return "Just now";
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,9 +70,39 @@ class HomeScreen extends StatelessWidget {
         alignment: Alignment.bottomLeft,
         children: [
           coverImage(),
-          const Positioned(
+          Positioned(
             top: Dimensions.topProfile,
-            child: ProfileHeader(),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: Dimensions.profileRadius,
+                    backgroundColor: Colors.grey.shade200,
+                    child: SvgPicture.asset(
+                      MyImages.profile,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    _name,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  Text(
+                    _timeAgo,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
